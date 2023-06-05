8b06984d6c19c28d98075e32ab51c1d5b85ea578
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-06-05 01:10:05 -0300

    :construction: Construção parcial do Splash

--- Habit/Splash/View/SplashView.swift
+++ Habit/Splash/View/SplashView.swift
@@ -9,24 +9,74 @@ import SwiftUI
 
 struct SplashView: View {
     
-    @State var state: SplashUIState = .loading
+    @State var state: SplashUIState = .goToSignInScreen
     
     var body: some View {
         switch state {
         case .loading:
-            Text("Loading")
+            loadingView()
+           
         case .goToSignInScreen:
             Text("Tela de Login")
         case .goToHomeScreen:
             Text("Tela Principal")
         case .error(let msg):
             Text("Erro: \(msg)")
+            loadingView(error: msg)
         }
     }
 }
 
-struct SplashView_Previews: PreviewProvider {
-    static var previews: some View {
-        SplashView()
+extension SplashView {
+    func loadingView(error: String? = nil) -> some View {
+        ZStack {
+                 Image("logoHabits")
+                     .resizable()
+                     .scaledToFit()
+                     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                     
+            if let error = error {
+                Text("")
+                    .alert(isPresented: .constant(true)) {
+                        Alert(
+                            title: Text("Habit"),
+                            message: Text(error),
+                            dismissButton: .default(Text("Fechar")){
+                                //  Fazer algo quando o alerta sumir
+                            })
+                    }
+            }
+            
+        }.background(Color.purple).ignoresSafeArea()
     }
 }
+
+
+
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView(state: .error("teste de erro no servidor"))
+    }
+}
+
+
+
+
+
+
+
+
+
+//Use dessa forma quando for usar o mesmo bloco em outros locais do código
+//Nesse caso no devera ser usado chamando a struct assim: LoadingView()
+//struct LoadingView : View {
+//    var body: some View {
+//        ZStack {
+//            Image("logoHabits")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        }.background(Color.red).ignoresSafeArea()
+//    }
+//}
---
+++ Habit/Splash/ViewModel/SplashViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewModel.swift
+//  Habit
+//
+//  Created by Artur Mac on 05/06/23.
+//
+
+import SwiftUI
